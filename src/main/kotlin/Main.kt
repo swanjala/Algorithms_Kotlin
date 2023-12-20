14f71c6c4386ea4132349e64162e23c2680dbe92
swanjala <wanjala.sam.s@gmail.com>
2023-12-20 11:02:23 +0100

    Add an option selector

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    println("Enter selection: ")
+    val stringInput = readlnOrNull()
+
+    stringInput?.let {
+        optionProcessor(it.toInt())
+    }
+}
+
+private fun optionProcessor(selection: Int) {
+    when(selection) {
+        1 -> {
+            println("Selected : Fizz-Buzz : $selection")
+        }
+    }
 }
