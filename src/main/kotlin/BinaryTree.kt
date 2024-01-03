11f65571104d412f3b41024e3ce0a20c5048f206
swanjala <wanjala.sam.s@gmail.com>
2024-01-03 15:36:52 +0100

    Deleting a node from a tree

--- src/main/kotlin/BinaryTree.kt
+++ src/main/kotlin/BinaryTree.kt
@@ -12,23 +12,22 @@ package org.example
       20  30   6
 */
 
-class Node (
+class Node(
     var value: Int,
     var leftChild: Node? = null,
     var rightChild: Node? = null
-){
+) {
     fun insert(element: Int) {
-        if(element > this.value) {
-            if(this.rightChild == null) {
+        if (element > this.value) {
+            if (this.rightChild == null) {
                 this.rightChild = Node(element)
             } else {
                 this.rightChild?.insert(element)// recursive insertion
             }
-        } else if(element < value) {
-            if(this.leftChild == null) {
+        } else if (element < value) {
+            if (this.leftChild == null) {
                 this.leftChild = Node(element)
-            }
-            else {
+            } else {
                 this.leftChild?.insert(element) // recursive insertion
             }
         }
@@ -39,17 +38,96 @@ class Node (
         this.value < element -> rightChild?.find(element)
         else -> this
     }
+
+    fun delete(element: Int) {
+        when {
+            element > value -> {
+                scan(element, this.rightChild, this)
+                println("Deleted from left subtree")
+            }
+
+            else -> {
+                removeNode(this, null)
+                println("Deleted nothing")
+            }
+        }
+    }
+
+    private fun scan(element: Int, node: Node?, parent: Node?) {
+        if (node == null) {
+            println("Does not exist")
+            return
+        }
+        when {
+            element > node.value -> scan(element, node.rightChild, node)
+            element < node.value -> scan(element, node.leftChild, node)
+            element == node.value -> removeNode(node, parent)
+        }
+    }
+
+    private fun removeNode(node: Node, parent: Node?) {
+        node.leftChild?.let { left ->
+            run {
+                node.rightChild?.let {
+                    removeTwoChildren(node)
+                } ?: removeOneChild(node, left)
+            }
+        } ?: run {
+            node.rightChild?.let { right -> removeOneChild(node, right) }
+        } ?: removeNone(node, parent)
+    }
+
+    private fun removeNone(node: Node, parent: Node?) {
+        parent?.let { parentNode ->
+            if (node == parentNode.leftChild) {
+                parentNode.leftChild = null
+            } else if (node == parentNode.rightChild) {
+                parentNode.rightChild = null
+            }
+        } ?: throw IllegalStateException("Cannot remove")
+    }
+
+    private fun removeOneChild(parent: Node, child: Node) {
+        parent.value = child.value
+        parent.leftChild = child.leftChild
+        parent.rightChild = child.rightChild
+    }
+
+    private fun removeTwoChildren(node: Node) {
+        val left = node.leftChild
+        left?.rightChild?.let {
+            val maximumParent = findParentOfMaxChild(left)
+            maximumParent
+        }
+    }
+
+    private fun findParentOfMaxChild(node: Node): Node {
+        return node.rightChild?.let { right ->
+            right.rightChild?.let {
+                findParentOfMaxChild(right)
+            } ?: node
+        } ?: throw IllegalArgumentException("right child is null")
+    }
 }
 
 fun main(args: Array<String>) {
     val tree = Node(60)
     val elements = arrayOf(30, 33, 40, 20, 30, 125, 70, 90, 6)
 
-    for(element in elements) {
+    for (element in elements) {
         tree.insert(element)
     }
 
     val node = tree.find(125)
+
+    println(node)
+
+    val deleted = tree.delete(125)
+
+    // Deleted node
+    println(deleted)
+
+    // Selected node
 
     node?.let {
         println(it.value)
